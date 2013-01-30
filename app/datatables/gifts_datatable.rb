4befb513c3b79b069f1323f1cf67627317c3eb57
artem-mindrov <artem.mindrov@gmail.com>
2013-01-30 12:00:56 +0400

    Multi-column filtering

--- app/datatables/gifts_datatable.rb
+++ app/datatables/gifts_datatable.rb
@@ -40,9 +40,29 @@ private
   def fetch_gifts
     gifts = Gift.order("#{sort_column} #{sort_direction}")
     gifts = gifts.page(page).per_page(per_page)
+
     if params[:sSearch].present?
-      gifts = gifts.where("id like :search", search: "%#{params[:sSearch]}%")
+      q = "%#{params[:sSearch]}%"
+      gifts = gifts.joins(:user).
+          joins("join items i on gifts.item_id = i.id").
+          where("gifts.id like ? or i.name like ? or users.name like ?", q, q, q)
+
+      gifts = gifts.where("users.name like ?", "%#{params[:sSearch_1]}%") if params[:sSearch_1].present?
+      gifts = gifts.where("i.name like ?", "%#{params[:sSearch_2]}%") if params[:sSearch_2].present?
+    else
+      if params[:sSearch_1].present?
+        gifts = gifts.joins(:user).where("users.name like ?", "%#{params[:sSearch_1]}%")
+      end
+
+      if params[:sSearch_2].present?
+        gifts = gifts.joins('join items i on gifts.item_id = i.id').where("i.name like ?", "%#{params[:sSearch_2]}%")
+      end
     end
+
+    if params[:sSearch_0].present?
+      gifts = gifts.where("gifts.id like :search", search: "%#{params[:sSearch_0]}%")
+    end
+
     gifts
   end
 
